401a74f0284adffa4955d2af1e906eafb627ce33
MoonJaeSeung <wotmdskfk32@naver.com>
2023-07-08 02:37:04 +0900

    Initial commit

---
+++ src/main/java/com/example/test/Controller.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+public class Controller {
+
+    @GetMapping("/show")
+    public String show(){
+        return "pList";
+    }
+}
+
+
